00a1b37f90f1698a096aac7e7ca2535d014ded1b
John Reiner <johnreiner1990@gmail.com>
2020-01-15 18:00:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,29 @@
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#   final = {}
+#
+# end
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-  final = {}
-  
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
